dd995fddd34245ecfb0d3904265d1f7161950a13
gushakov <Georgy.Ushakov@unil.ch>
2019-07-24 18:24:34 +0200

    permission summary id auto-increment

--- axon-multi-projection/src/main/java/com/github/axonmulti/projection/entity/PersonSummary.java
+++ axon-multi-projection/src/main/java/com/github/axonmulti/projection/entity/PersonSummary.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 public class PersonSummary {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String personId;
